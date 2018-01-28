1296c272959dbcab1c11b36a02b5564c9b9f7ecf
myoneda <hydrangea966@gmail.com>
2018-01-28 16:30:02 +0900

    def

--- bento_regression.py
+++ bento_regression.py
@@ -26,11 +26,22 @@ def get_dummy_encoded_datetime_df(df):
 
 
 def prepare(df):
-    # fillna to 0
-    df.fillna(-1, inplace=True)
+    df.fillna(0, inplace=True)
 
-    dummy_datetime_df = get_dummy_encoded_datetime_df(df)
-    df = df.join(dummy_datetime_df)
+    get_year = lambda x: str(x.year)
+    year = df.datetime.map(get_year)
+    get_month = lambda x: str(x.month)
+    month = df.datetime.map(get_month)
+    get_day = lambda x: str(x.day)
+    day = df.datetime.map(get_day)
+    splitted_datetime_df = pd.DataFrame({
+        'year': year,
+        'month': month,
+        'day': day, },
+        columns=['year', 'month', 'day']
+    )
+    dummy_df = pd.get_dummies(splitted_datetime_df[['year', 'month', 'day']])
+    df = df.join(dummy_df)
 
     dummy_week_df = pd.get_dummies(df[['week']])
     df = df.join(dummy_week_df)
@@ -38,7 +49,10 @@ def prepare(df):
     df = df.join(dummy_menu_df)
 
     # 特記事項 文字列が入っている。フラグにする
+    df.remarks.fillna(0, inplace=True)
     df.remarks.where(df.remarks == 0, 1, inplace=True)
+
+    df.payday.fillna(0, inplace=True)
 
     df.event.fillna(-1, inplace=True)
     df.event.where(df.event == -1, 1, inplace=True)
